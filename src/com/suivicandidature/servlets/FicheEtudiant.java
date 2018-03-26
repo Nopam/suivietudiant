afb11c107920caf01db038f3ed5ad0cdb68c5fce
Nana2.0 <marguodu95@hotmail.fr>
2018-03-26 09:19:48 +0200

    Données fiche etudiant

--- src/com/suivicandidature/servlets/FicheEtudiant.java
+++ src/com/suivicandidature/servlets/FicheEtudiant.java
@@ -7,13 +7,32 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.suivicandidature.classes.*;
 
-public class Modules extends HttpServlet {
+public class FicheEtudiant extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
 		
-		this.getServletContext().getRequestDispatcher( "/WEB-INF/vues/modules.jsp" ).forward( request, response );
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/vues/ficheEtudiant.jsp" ).forward( request, response );
 
 	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			 throws ServletException, IOException { 
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/vues/ficheEtudiant.jsp" ).forward( request, response );
+
+			
+			 String nom = request.getParameter("name");
+			 String prenom = request.getParameter("surname");
+			 int dateNaiss = 3;
+			 String mail = request.getParameter("mail");
+			 String tel = request.getParameter("tel");
+			 String statut = request.getParameter("statut");
+			 
+			 Candidat e1 = new Candidat(nom,prenom,dateNaiss,mail,null);
+			 System.out.println(e1);;
+			 
+			 
+			}
 }
